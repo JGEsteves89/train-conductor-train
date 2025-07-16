9c340bcf16b5ca08a020b34270e0f5b688f692eb
José Esteves <jose.esteves.mail@gmail.com>
2025-07-16 17:27:31 +0100

    This is something

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
 	<v-app>
 		<v-app-bar app color="primary" dark class="app-bar-no-padding">
 			<img :src="trainImage" alt="Logo" class="logo-img" />
-
 			<v-spacer></v-spacer>
-
 			<v-btn @click="$router.push('/')" text>
 				<span class="mr-2">Home</span>
 			</v-btn>
@@ -13,36 +11,26 @@
 		<v-main>
 			<router-view />
 		</v-main>
+
+		<v-footer app color="grey lighten-4">
+			<v-container class="text-center">
+				<small>
+					Contact: <a href="mailto:ijimiguel@gmail.com">ijimiguel@gmail.com</a>
+					&nbsp;|&nbsp; Version: {{ version }}
+				</small>
+			</v-container>
+		</v-footer>
 	</v-app>
 </template>
 
-<style scoped>
-.app-bar-no-padding {
-	padding: 0 !important;
-}
-
-/* Use deep selector to reach internal Vuetify class */
-.app-bar-no-padding /deep/ .v-toolbar__content {
-	padding: 0 !important;
-}
-
-/* Ensures the img is flush and full-height */
-.logo-img {
-	height: 64px;
-	/* or match your toolbar's exact height (e.g. 56px or 64px) */
-	width: auto;
-	margin: 0;
-	padding: 0;
-	display: block;
-	/* eliminate inline spacing */
-}
-</style>
 <script>
+// Import version from package.json (only works if Vue CLI with webpack)
+import pkg from "../package.json";
+
 export default {
 	name: "App",
-
 	data: () => ({
-		//
+		version: pkg.version,
 	}),
 	computed: {
 		trainImage() {
@@ -51,3 +39,21 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+.app-bar-no-padding {
+	padding: 0 !important;
+}
+
+.app-bar-no-padding /deep/ .v-toolbar__content {
+	padding: 0 !important;
+}
+
+.logo-img {
+	height: 64px;
+	width: auto;
+	margin: 0;
+	padding: 0;
+	display: block;
+}
+</style>
